869e22cdf14af00e0828351c487020c0e1d60358
Lorenzo99 <lorenzopouget99@hotmail.fr>
2025-12-07 15:00:52 +0100

    Ajout Repository Mangas

---
+++ src/main/java/list/perso/PopCulture/repo/MangasRepository.java
@@ -0,0 +1,9 @@
+package list.perso.PopCulture.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import list.perso.PopCulture.model.Mangas;
+
+public interface MangasRepository extends JpaRepository<Mangas,Integer> {
+    
+}
