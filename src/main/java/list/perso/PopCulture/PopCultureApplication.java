3836bfdb990b22fad652b5357fe5ddc6fc9e1143
Lorenzo99 <lorenzopouget99@hotmail.fr>
2025-12-07 14:33:50 +0100

    Initial Commit

---
+++ src/main/java/list/perso/PopCulture/PopCultureApplication.java
@@ -0,0 +1,13 @@
+package list.perso.PopCulture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopCultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/list/perso/PopCulture/PopCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package list.perso.PopCulture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
